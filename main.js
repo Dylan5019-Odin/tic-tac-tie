4893b9ecbfc01f06be3be423fa8abf6aeaf54300
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-11 15:04:01 -0600

    Switched 3x3 grid creation to use JS

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 
+
 // Using a Module to setup the Game Board
 const gameBoard  = (() => {
     const board = [
@@ -20,5 +21,35 @@ const playerFactory = (name, symb) => {
     return {name, symb}
 };
 
+
+// Using a Module to setup the display Controller
+const displayController  = (() => {
+    //const board =  gameBoard.board;
+
+    const displayBoard  = () => {
+      let gameContainer = document.getElementById("gameContainer");
+
+      for (let i = 0; i < 9; i ++) {
+        let cell = document.createElement("div");
+        cell.classList = "cell";
+        cell.id = `cell_${i}`;
+        gameContainer.append(cell);
+      }
+    }
+
+    return {
+      displayBoard,
+    };
+
+})();
+
+
+
 const player1 = playerFactory(`Player1`, `X`);
 const player2 = playerFactory(`Player2`, `O`);
+
+displayController.displayBoard();
+
+
+
+
