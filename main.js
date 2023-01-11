0899e762348f61844dd36c96c0f6b5b6551d4496
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-11 15:49:22 -0600

    Displays the grid based on the game board, and added some helper functions

--- main.js
+++ main.js
@@ -1,55 +1,72 @@
-
-
-
 // Using a Module to setup the Game Board
-const gameBoard  = (() => {
-    const board = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""]
-    ];
+const gameBoard = (() => {
+  const board = [
+    ["O", "X", "X"],
+    ["O", "X", "O"],
+    ["X", "O", "X"],
+  ];
 
-    return {
-        board
-    };
+  const updateBoard = (i, j, newValue) => {
+    [board[i][j] = newValue];
+  }
 
+  return {
+    board,
+    updateBoard,
+  };
 })();
 
 
 // Using a Factory to setup the Players
 const playerFactory = (name, symb) => {
-    return {name, symb}
+  return { name, symb };
 };
 
-
 // Using a Module to setup the display Controller
-const displayController  = (() => {
-    //const board =  gameBoard.board;
+const displayController = (() => {
 
-    const displayBoard  = () => {
-      let gameContainer = document.getElementById("gameContainer");
+const getCurrentCellEvent = () => {
+    let currentCell = [];
+    document.querySelectorAll(".cell").forEach(cell => {
+     cell.addEventListener("click", () => {
+        currentCell = cell.id.split("_");
+        currentCell.shift();       
+     });
 
-      for (let i = 0; i < 9; i ++) {
+     return currentCell;
+  });
+ 
+}
+
+// Generates the Grid Based off the gameBoard
+  const generateGrid = () => {
+    let gameContainer = document.getElementById("gameContainer");
+    gameContainer.innerHTML = '';
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
         let cell = document.createElement("div");
         cell.classList = "cell";
-        cell.id = `cell_${i}`;
+        cell.id = `cell_${i}_${j}`;
+        cell.textContent = gameBoard.board[i][j];
         gameContainer.append(cell);
       }
     }
+  };
 
-    return {
-      displayBoard,
-    };
+  const displayBoard = () => {
+    generateGrid();
+    getCurrentCellEvent();
+  };
 
+  return {
+    displayBoard,
+  };
 })();
-
-
 
 const player1 = playerFactory(`Player1`, `X`);
 const player2 = playerFactory(`Player2`, `O`);
 
 displayController.displayBoard();
-
-
 
 
